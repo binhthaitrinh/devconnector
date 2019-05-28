8642543744cfa53246491da924bcc2a985ebb94f
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-05-28 11:32:39 -0700

    Customer Auth Middleware & JWT Verify

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,19 @@
 const express = require('express');
-
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 const router = express.Router();
 
 // @route           GET api/auth
 // @description     Test route
 // @access          Public
-router.get('/', (req, res) => res.send('Ath route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
